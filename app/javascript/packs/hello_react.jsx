f914c56f5c82d33b0aa9af4e265992abca4daeca
seb1441 <sebastienauriault@gmail.com>
2017-10-07 17:24:26 -0400

    Add Quotes API

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -20,7 +20,7 @@ Hello.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Hello name="Test React" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
